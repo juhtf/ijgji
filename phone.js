81ac476f91c20da6aafe8f8702072833a4806d06
juhtf <86907413+juhtf@users.noreply.github.com>
2022-02-06 14:34:18 +0800

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,20 @@
+img=" ";
+
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+}
+
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("dog",45,75);
+    noFill();
+    stroke("green");
+    rect(30,60,450,350);
+}
